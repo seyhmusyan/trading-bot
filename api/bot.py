9340f246f1359133017998bd8920c2a95ae54710
seyhmusyan <90778219+seyhmusyan@users.noreply.github.com>
2025-12-10 16:48:58 +0200

    Update bot.py

--- api/bot.py
+++ api/bot.py
@@ -1,1 +1,14 @@
-
+{
+  "version": 2,
+  "functions": {
+    "api/bot.py": {
+      "runtime": "python3.9"
+    }
+  },
+  "routes": [
+    {
+      "src": "/api/bot",
+      "dest": "/api/bot.py"
+    }
+  ]
+}
